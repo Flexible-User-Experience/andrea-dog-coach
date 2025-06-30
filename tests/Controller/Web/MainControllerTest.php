4041596f3739be3ee00014c527f29c581083ac4e
David Romaní <david@flux.cat>
2025-06-30 18:23:03 +0200

    add more testings

--- tests/Controller/Web/MainControllerTest.php
+++ tests/Controller/Web/MainControllerTest.php
@@ -28,5 +28,13 @@ class MainControllerTest extends WebTestCase
         self::assertResponseIsSuccessful();
         $client->request(Request::METHOD_GET, sprintf('/%s%s', LocaleEnum::de, RoutesEnum::app_web_services_path_de));
         self::assertResponseIsSuccessful();
+        $client->request(Request::METHOD_GET, RoutesEnum::app_web_contact_us_path_ca);
+        self::assertResponseIsSuccessful();
+        $client->request(Request::METHOD_GET, sprintf('/%s%s', LocaleEnum::es, RoutesEnum::app_web_contact_us_path_es));
+        self::assertResponseIsSuccessful();
+        $client->request(Request::METHOD_GET, sprintf('/%s%s', LocaleEnum::en, RoutesEnum::app_web_contact_us_path_en));
+        self::assertResponseIsSuccessful();
+        $client->request(Request::METHOD_GET, sprintf('/%s%s', LocaleEnum::de, RoutesEnum::app_web_contact_us_path_de));
+        self::assertResponseIsSuccessful();
     }
 }
