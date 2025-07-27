7e7bcbbffdc3d67062e93e2bd9c3214550cf9d6f
David Romaní <david@flux.cat>
2025-07-27 12:20:31 +0200

    better testing

--- tests/Controller/Web/MainControllerTest.php
+++ tests/Controller/Web/MainControllerTest.php
@@ -6,6 +6,7 @@ use App\Enum\LocaleEnum;
 use App\Enum\RoutesEnum;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainControllerTest extends WebTestCase
 {
@@ -44,5 +45,18 @@ class MainControllerTest extends WebTestCase
         self::assertResponseIsSuccessful();
         $client->request(Request::METHOD_GET, sprintf('/%s%s', LocaleEnum::de, RoutesEnum::app_web_contact_us_path_de));
         self::assertResponseIsSuccessful();
+    }
+
+    public function testNotFound(): void
+    {
+        $client = static::createClient();
+        $client->request(Request::METHOD_GET, str_replace('{slug}', 'service-seven', sprintf('%s', RoutesEnum::app_web_service_detail_path_ca)));
+        self::assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+        $client->request(Request::METHOD_GET, str_replace('{slug}', 'service-seven', sprintf('/%s%s', LocaleEnum::es, RoutesEnum::app_web_service_detail_path_es)));
+        self::assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+        $client->request(Request::METHOD_GET, str_replace('{slug}', 'service-seven', sprintf('/%s%s', LocaleEnum::en, RoutesEnum::app_web_service_detail_path_en)));
+        self::assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+        $client->request(Request::METHOD_GET, str_replace('{slug}', 'service-seven', sprintf('/%s%s', LocaleEnum::de, RoutesEnum::app_web_service_detail_path_de)));
+        self::assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
     }
 }
