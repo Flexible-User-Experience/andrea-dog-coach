8d57cca3879e0522e48246d206e437057b61dcaf
David Romaní <david@flux.cat>
2025-07-29 18:17:10 +0200

    enable persist send message

--- src/Controller/Web/MainController.php
+++ src/Controller/Web/MainController.php
@@ -105,7 +105,7 @@ final class MainController extends AbstractController
         $form = $this->createForm(ContactMessageFormType::class, $contactMessage);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // TODO until reCaptcha is not enabled $cmr->add(entity: $contactMessage, flush: true);
+            $cmr->add(entity: $contactMessage, flush: true);
             $mm->sendNewContactMessageNotificationToManager($contactMessage);
             $this->addFlash(
                 'info',
